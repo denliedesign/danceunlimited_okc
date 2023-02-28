60bfb3154b12ff8c9ecde60c936f7470fc0f93f6
denliedesign <customdenlie@gmail.com>
2023-02-27 18:21:07 -0600

    update summer sched and remove pop ups

--- resources/views/du-pop-ups.blade.php
+++ resources/views/du-pop-ups.blade.php
@@ -8,19 +8,20 @@
 
     <div class="container py-5">
         <h1 class="text-center"><strong>DU Pop Ups</strong></h1>
+        <p class="text-center">Check back in Aug 2023</p>
 
-        <div class="row mt-5">
-            <div class="col-sm-1"></div>
-            <div class="col-sm my-3"><img src="/images-snow/camps-1.jpg" alt="summer camp info" class="img-fluid shadow rounded"></div>
-            <div class="col-sm my-3"><img src="/images-snow/camps-3.jpg" alt="summer camp info" class="img-fluid shadow rounded"></div>
-            <div class="col-sm my-3"><img src="/images-snow/doll-camp.jpeg" alt="summer camp info" class="img-fluid shadow rounded"></div>
-            <div class="col-sm-1"></div>
-        </div>
-        <div class="d-flex justify-content-center">
-            <div class="col-sm-2"></div>
-            <div class="col-sm my-3"><img src="/images-snow/camps-6.jpeg" alt="summer camp info" class="img-fluid shadow rounded"></div>
-            <div class="col-sm-2"></div>
-        </div>
+{{--        <div class="row mt-5">--}}
+{{--            <div class="col-sm-1"></div>--}}
+{{--            <div class="col-sm my-3"><img src="/images-snow/camps-1.jpg" alt="summer camp info" class="img-fluid shadow rounded"></div>--}}
+{{--            <div class="col-sm my-3"><img src="/images-snow/camps-3.jpg" alt="summer camp info" class="img-fluid shadow rounded"></div>--}}
+{{--            <div class="col-sm my-3"><img src="/images-snow/doll-camp.jpeg" alt="summer camp info" class="img-fluid shadow rounded"></div>--}}
+{{--            <div class="col-sm-1"></div>--}}
+{{--        </div>--}}
+{{--        <div class="d-flex justify-content-center">--}}
+{{--            <div class="col-sm-2"></div>--}}
+{{--            <div class="col-sm my-3"><img src="/images-snow/camps-6.jpeg" alt="summer camp info" class="img-fluid shadow rounded"></div>--}}
+{{--            <div class="col-sm-2"></div>--}}
+{{--        </div>--}}
     </div>
 
 @endsection
