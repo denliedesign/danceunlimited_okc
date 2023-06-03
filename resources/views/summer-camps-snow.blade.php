1b75db690df831a9fc36e8b5ec9cd44529a32575
denliedesign <customdenlie@gmail.com>
2023-06-03 10:33:27 -0500

    new summer schedule and remove yellow camps

--- resources/views/summer-camps-snow.blade.php
+++ resources/views/summer-camps-snow.blade.php
@@ -82,19 +82,19 @@
             {{--            <div class="col-sm my-3"><img src="/images-snow/spotlight-summer-musicals-3.jpg" alt="summer camp info" class="img-fluid shadow rounded"></div>--}}
             {{--            <div class="col-sm my-3"><img src="/images-snow/summer-camps.jpg" alt="summer camp info" class="img-fluid shadow rounded"></div>--}}
         </div>
-        <div class="row my-2">
-            <div class="col-sm-2"></div>
-            <div class="col-sm my-3"><img src="/images-snow/camps-7.jpeg" alt="summer camp info"
-                                          class="img-fluid shadow rounded">
-                <div class="d-flex justify-content-center mt-1">
-                    <div class="btn btn-light shadow px-5" style="border-radius: 20px;">
-                        <a href="https://app.thestudiodirector.com/danceunlimitedpac/portal.sd" target="_blank"
-                           class="text-decoration-none text-dark fw-bold">Enroll Now</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-2"></div>
-        </div>
+{{--        <div class="row my-2">--}}
+{{--            <div class="col-sm-2"></div>--}}
+{{--            <div class="col-sm my-3"><img src="/images-snow/camps-7.jpeg" alt="summer camp info"--}}
+{{--                                          class="img-fluid shadow rounded">--}}
+{{--                <div class="d-flex justify-content-center mt-1">--}}
+{{--                    <div class="btn btn-light shadow px-5" style="border-radius: 20px;">--}}
+{{--                        <a href="https://app.thestudiodirector.com/danceunlimitedpac/portal.sd" target="_blank"--}}
+{{--                           class="text-decoration-none text-dark fw-bold">Enroll Now</a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--            <div class="col-sm-2"></div>--}}
+{{--        </div>--}}
     </div>
 
 @endsection
