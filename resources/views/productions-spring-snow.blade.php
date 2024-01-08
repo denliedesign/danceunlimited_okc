0eee20c1d8867b4f5550fd5a4da2e1993dc9ee1d
denliedesign <customdenlie@gmail.com>
2024-01-08 08:29:27 -0600

    pic sched update

--- resources/views/productions-spring-snow.blade.php
+++ resources/views/productions-spring-snow.blade.php
@@ -42,7 +42,7 @@
             <p class="text-center">
                 Every year we have a special Recital Picture Day where dancers can have their portraits taken professionally at the studio with Spotlight Dance Photography. Always amazing photos! Dancers are not required to purchase photos, but they are a wonderful keepsake for dancers and families for years to come.
                 <br><br>
-                <a href="/images/encanto-picture.pdf" target="_blank">Encanto/Aftershow Picture Schedule</a>
+                <a href="/images/2024-pic-sched.pdf" target="_blank">Encanto/Aftershow Picture Schedule</a>
             </p>
 {{--            <div class="d-flex justify-content-center">--}}
 {{--                <div class="btn btn-light shadow px-5" style="border-radius: 20px;">--}}
