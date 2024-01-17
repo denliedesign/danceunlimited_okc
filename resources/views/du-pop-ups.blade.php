0c2f05a5d071c48d921453279e2549203e9e18e6
denliedesign <customdenlie@gmail.com>
2024-01-17 10:42:09 -0600

    pop ups

--- resources/views/du-pop-ups.blade.php
+++ resources/views/du-pop-ups.blade.php
@@ -8,15 +8,14 @@
 
     <div class="container py-5">
         <h1 class="text-center"><strong>DU Pop Ups</strong></h1>
-        <p class="text-center">Coming Jan 15th</p>
+{{--        <p class="text-center">Coming Jan 15th</p>--}}
 
-{{--        <div class="row mt-5">--}}
-{{--            <div class="col-sm-1"></div>--}}
-{{--            <div class="col-sm my-3"><img src="/images-snow/camps-1.jpg" alt="summer camp info" class="img-fluid shadow rounded"></div>--}}
-{{--            <div class="col-sm my-3"><img src="/images-snow/camps-3.jpg" alt="summer camp info" class="img-fluid shadow rounded"></div>--}}
+        <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2 mt-5">
+            <div class="col-sm my-3"><img src="/images/pop-up-pink.png" alt="camp info" class="img-fluid shadow rounded"></div>
+            <div class="col-sm my-3"><img src="/images/pop-up-eras.png" alt="camp info" class="img-fluid shadow rounded"></div>
 {{--            <div class="col-sm my-3"><img src="/images-snow/doll-camp.jpeg" alt="summer camp info" class="img-fluid shadow rounded"></div>--}}
 {{--            <div class="col-sm-1"></div>--}}
-{{--        </div>--}}
+        </div>
 {{--        <div class="d-flex justify-content-center">--}}
 {{--            <div class="col-sm-2"></div>--}}
 {{--            <div class="col-sm my-3"><img src="/images-snow/camps-6.jpeg" alt="summer camp info" class="img-fluid shadow rounded"></div>--}}
