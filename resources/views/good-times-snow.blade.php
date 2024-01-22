8f54f3396077e3112a9062ceb584449053e6857c
denliedesign <customdenlie@gmail.com>
2024-01-22 08:55:19 -0600

    good times pics

--- resources/views/good-times-snow.blade.php
+++ resources/views/good-times-snow.blade.php
@@ -8,10 +8,15 @@
 
     <div class="container py-5">
         <h1 class="text-center"><strong>Good Times</strong></h1>
-
-        <p class="text-center">
-            Photo gallery coming soon
-        </p>
+{{--        <p class="text-center">--}}
+{{--            Photo gallery coming soon--}}
+{{--        </p>--}}
+        <div class="row row-cols-1 row-cols-sm-1 row-cols-md-3 row-cols-lg-3">
+            <div class="col my-2"><img src="/images/good-2.jpg" alt="dancers posing together" class="img-fluid shadow rounded"></div>
+            <div class="col my-2"><img src="/images/good-3.jpg" alt="dancers posing together" class="img-fluid shadow rounded"></div>
+            <div class="col my-2"><img src="/images/good-4.jpg" alt="dancers posing together" class="img-fluid shadow rounded"></div>
+            <div class="col my-2"><img src="/images/good-1.jpg" alt="dancers posing together" class="img-fluid shadow rounded"></div>
+        </div>
     </div>
 
 @endsection
