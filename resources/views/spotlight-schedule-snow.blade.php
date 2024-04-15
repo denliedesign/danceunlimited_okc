684278857db0e50d2548e989a1bf366fd276efd5
denliedesign <customdenlie@gmail.com>
2024-04-15 12:09:29 -0500

    fall dance and spotlight classes

--- resources/views/spotlight-schedule-snow.blade.php
+++ resources/views/spotlight-schedule-snow.blade.php
@@ -8,12 +8,15 @@
 
     <div class="container py-5">
         <h1 class="text-center"><strong>Spotlight Acting Class Schedule</strong></h1>
-        <p class="text-center">
-            Summer Info coming end of Jan 2024
-            <br><br>
-            <a href="https://docs.google.com/document/d/1nT3Ge3RMpVZPeYLu3CjsZirPcCsffg_ryB70dAg-i-M/edit" target="_blank">Acting Spring 2024 Schedule</a>
-            <br><br>
-        </p>
+        <div class="d-flex justify-content-center my-4">
+            <img src="/images/spotlight-classes-24.jpg" alt="" class="img-fluid">
+        </div>
+{{--        <p class="text-center">--}}
+{{--            Summer Info coming end of Jan 2024--}}
+{{--            <br><br>--}}
+{{--            <a href="https://docs.google.com/document/d/1nT3Ge3RMpVZPeYLu3CjsZirPcCsffg_ryB70dAg-i-M/edit" target="_blank">Acting Spring 2024 Schedule</a>--}}
+{{--            <br><br>--}}
+{{--        </p>--}}
         <div class="row row-cols-1 row-cols-sm-1 row-cols-md-3 row-cols-lg-3">
             <div class="col-sm d-flex align-items-center"><img src="/images/spot-1.jpg" alt="actors" class="img-fluid shadow rounded"></div>
             <div class="col-sm d-flex align-items-center"><img src="/images/spot-2.jpg" alt="actors" class="img-fluid shadow rounded"></div>
