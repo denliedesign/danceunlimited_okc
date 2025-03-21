44f77a8fa4d80ad03fb1e0c53430b27d24227950
denliedesign <customdenlie@gmail.com>
2025-03-21 09:20:58 -0500

    move du logo to center

--- resources/views/welcome-snow.blade.php
+++ resources/views/welcome-snow.blade.php
@@ -3,8 +3,8 @@
 
 <div class="container px-4 py-5">
     <div class="row d-flex align-items-center">
-        <div class="col d-flex justify-content-center"><img src="/images-snow/logo-new-du.png" alt="logo" class="img-fluid"></div>
         <div class="col d-flex justify-content-center"><img src="/images/logo-25-vibe.jpeg" alt="logo" class="img-fluid"></div>
+        <div class="col d-flex justify-content-center"><img src="/images-snow/logo-new-du.png" alt="logo" class="img-fluid"></div>
         <div class="col d-flex justify-content-center"><img src="/images-snow/logo-new-spotlight.png" alt="logo" class="img-fluid"></div>
     </div>
 </div>
