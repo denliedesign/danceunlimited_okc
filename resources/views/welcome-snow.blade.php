3b38c0183985de494ddf76f6a8acb7a0f122a60c
denliedesign <customdenlie@gmail.com>
2025-07-10 12:09:10 -0500

    clickable logos

--- resources/views/welcome-snow.blade.php
+++ resources/views/welcome-snow.blade.php
@@ -3,9 +3,9 @@
 
 <div class="container px-4 py-5">
     <div class="row d-flex align-items-center">
-        <div class="col d-flex justify-content-center"><img src="/images/logo-25-vibe.jpeg" alt="logo" class="img-fluid"></div>
-        <div class="col d-flex justify-content-center"><img src="/images-snow/logo-new-du.png" alt="logo" class="img-fluid"></div>
-        <div class="col d-flex justify-content-center"><img src="/images-snow/logo-new-spotlight.png" alt="logo" class="img-fluid"></div>
+        <div class="col d-flex justify-content-center"><a href="/vibe-audition"><img src="/images/logo-25-vibe.jpeg" alt="logo" class="img-fluid"></a></div>
+        <div class="col d-flex justify-content-center"><a href="/class-offerings"><img src="/images-snow/logo-new-du.png" alt="logo" class="img-fluid"></a></div>
+        <div class="col d-flex justify-content-center"><a href="/spotlight-schedule"><img src="/images-snow/logo-new-spotlight.png" alt="logo" class="img-fluid"></a></div>
     </div>
 </div>
 
