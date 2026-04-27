db3859b0fb0953aecaf54dadfba89137b220485d
denliedesign <customdenlie@gmail.com>
2026-04-27 14:47:56 -0500

    summer schedule link fix again

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::get('fall-schedule', function () {
 //    return view('fall-tuition-snow');
 //});
 Route::get('/summer-schedule', function () {
-    return view('/summer-schedule-snow');
+    return view('summer-schedule-snow');
 });
 //Route::get('summer-camps', function () {
 //    return view('summer-camps-snow');
